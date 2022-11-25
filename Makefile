eade3ec8164c706af17f6530b42ce4faa51ea36e
chandravesh chaudhari <chandraveshchaudhari@gmail.com>
2022-11-25 11:01:58 +0530

    Initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+dev:
+	pip install -r backend/requirements.txt
+	cd frontend && yarn install --dev
+
+migrate:
+	python backend/manage.py makemigrations
+	python backend/manage.py migrate
+
+run:
+	yarn --cwd frontend start & python backend/manage.py runserver
+cd
---
+++ backend/graphqlfiles/foundation/types.py
@@ -0,0 +1,15 @@
+from graphene_django import DjangoObjectType
+from foundation.models import Note
+
+
+class NoteType(DjangoObjectType):
+
+    class Meta:
+        model = Note
+        only_fields = (
+            'id',
+            'title',
+            'body',
+            'created_at',
+        )
+        use_connection = True
